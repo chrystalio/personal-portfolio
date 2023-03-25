bc1a3bb64eb9dc94fbfbd9a45a3573e705c9a47b
chrystalio <chrystalio.k@outlook.com>
2023-03-26 03:20:53 +0700

    Add behavior navbar for desktop

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,4 +1,5 @@
 const hamburger = document.querySelector('#hamburger');
+const navMenu = document.querySelector('#nav-menu');
 
 // Navbar fixed
 window.onscroll = () => {
@@ -16,4 +17,5 @@ window.onscroll = () => {
 // Hamburger
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('hamburger-active');
+    navMenu.classList.toggle('hidden');
 });
