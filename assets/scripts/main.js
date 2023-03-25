b0ef9af9aef87f4020196c9be73dd3812bb687cc
chrystalio <chrystalio.k@outlook.com>
2023-03-26 01:59:45 +0700

    Add navbar fixed script

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,5 +1,19 @@
 const hamburger = document.querySelector('#hamburger');
 
+// Navbar fixed
+window.onscroll = () => {
+    const header = document.querySelector('header');
+    const fixedNav = header.offsetTop;
+
+    if (window.pageYOffset > fixedNav) {
+        header.classList.add('navbar-fixed');
+    } else {
+        header.classList.remove('navbar-fixed');
+    }
+}
+
+
+// Hamburger
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('hamburger-active');
 });
